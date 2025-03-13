3e96f24f82ed7860f4dc6d8c1578ff9b02f9c318
SantiCoVe <cottevelasquez@gmail.com>
2025-03-13 17:01:56 -0500

    feat: Responsive model almost finished

--- src/pages/ExperienciaProfesional/ExperienciaP.jsx
+++ src/pages/ExperienciaProfesional/ExperienciaP.jsx
@@ -1,10 +1,21 @@
-import { Section_E_Es } from "../../layout/Section_E_Es/Section_E_Es"
+import { Div_E_Es } from "../../layout/Div_E_Es/Div_E_Es"
 import './ExperienciaP.css'
 export const ExperienciaP = () => {
     return (
         <main className="main-E">
             <h1>Experiencia Profesional</h1>
-            <Section_E_Es/>
+            <section>
+                <Div_E_Es contentTitle='Libro404' 
+                    contentp='Este proyecto se trabajo con la metologia scrum, 
+                    donde participe desarrollando el backend, este proyecto se trataba de una 
+                    biblioteca donde se podria buscar el libro que desee.'
+                    link='https://libro404-organization.github.io/libro404/'/>
+                        
+                <Div_E_Es contentTitle='Diccionario'
+                    contentp='Este proyecto lo elabore solo donde se le pasa una palabra 
+                    que este en el traductor y la traduce sea de español a ingles o lo contrario'
+                    link='https://munoz2006.github.io/diccionario/'/>
+            </section>
         </main>
     )
 }
